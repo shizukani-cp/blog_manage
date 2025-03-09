956819d1baf70e7f7f6af0b146f983686445b63e
shizukani-cp <to.maimai0606@gmail.com>
2025-03-09 15:40:00 +0900

    XML宣言が先頭に来るよう変更

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from jinja2 import Template
 ENCODE = "utf-8"
 EXTENSIONS = ["tables", "fenced_code"]
 
-RSS_TEMPLATE = """
-<?xml version="1.0" encoding="UTF-8" ?>
+RSS_TEMPLATE = """<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
   <title>静カニのブログ</title>
